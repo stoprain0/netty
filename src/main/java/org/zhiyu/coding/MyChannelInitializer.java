485d66286925490406e4c4849721dc0dcbb57d9c
lingziyu <2304465670@qq.com>
2024-08-19 16:45:31 +0800

    字符串解码器解决半包粘包问题

--- src/main/java/org/zhiyu/coding/MyChannelInitializer.java
+++ src/main/java/org/zhiyu/coding/MyChannelInitializer.java
@@ -2,16 +2,25 @@ package org.zhiyu.coding;
 
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+import java.nio.charset.Charset;
 
 public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel channel) throws Exception {
-        System.out.println("链接报告开始");
-        System.out.println("链接报告信息：有一客户端链接到本服务端");
-        System.out.println("链接报告IP:" + channel.localAddress().getHostString());
-        System.out.println("链接报告Port:" + channel.localAddress().getPort());
-        System.out.println("链接报告完毕");
-
+        /* 解码器 */
+        //基于换行符号
+        channel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+        //基于指定字符串【换行符，功能等同于上面】
+//        channel.pipeline().addLast(new DelimiterBasedFrameDecoder(1024,false, Delimiters.lineDelimiter()))
+        //基于最大长度
+//        channel.pipeline().addLast(new FixedLengthFrameDecoder(4));
+        channel.pipeline().addLast(new StringDecoder(Charset.forName("GBK")));
         //在管道中添加我们自己的接收数据实现方法
         channel.pipeline().addLast(new MyServerHandler());
     }
--- src/main/java/org/zhiyu/coding/MyServerHandler.java
+++ src/main/java/org/zhiyu/coding/MyServerHandler.java
@@ -3,20 +3,34 @@ package org.zhiyu.coding;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.socket.SocketChannel;
 
 import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MyServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        SocketChannel channel = (SocketChannel) ctx.channel();
+        System.out.println("链接报告开始 {公众号：bugstack虫洞栈 >获取学习源码}");
+        System.out.println("链接报告信息：有一客户端链接到本服务端");
+        System.out.println("链接报告IP:" + channel.localAddress().getHostString());
+        System.out.println("链接报告Port:" + channel.localAddress().getPort());
+        System.out.println("链接报告完毕");
+    }
+
+    @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         //接收msg消息
-        ByteBuf buf = (ByteBuf) msg;
-        byte[] msgByte = new byte[buf.readableBytes()];
-        buf.readBytes(msgByte);
-        System.out.print(new Date() + "接收到消息：");
-        System.out.println(new String(msgByte, Charset.forName("GBK")));
+//        ByteBuf buf = (ByteBuf) msg;
+//        byte[] msgByte = new byte[buf.readableBytes()];
+//        buf.readBytes(msgByte);
+//        System.out.print(new Date() + "接收到消息：");
+//        System.out.println(new String(msgByte, Charset.forName("GBK")));
 
+        //接收msg消息{与上一章节相比，此处已经不需要自己进行解码}
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + " 接收到消息：" + msg);
     }
 }
