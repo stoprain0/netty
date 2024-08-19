8c72b6d2c646bf7cb25c3cf28673a4ab939fa411
lingziyu <2304465670@qq.com>
2024-08-19 17:37:38 +0800

    NettyClient

--- src/main/java/org/zhiyu/coding/MyChannelInitializer.java
+++ src/main/java/org/zhiyu/coding/MyChannelInitializer.java
@@ -2,9 +2,6 @@ package org.zhiyu.coding;
 
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.DelimiterBasedFrameDecoder;
-import io.netty.handler.codec.Delimiters;
-import io.netty.handler.codec.FixedLengthFrameDecoder;
 import io.netty.handler.codec.LineBasedFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
@@ -14,13 +11,13 @@ import java.nio.charset.Charset;
 public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel channel) throws Exception {
-        //基于换行符号
+        // 基于换行符号
         channel.pipeline().addLast(new LineBasedFrameDecoder(1024));
-        //解码器，收数据时起作用
+        // 解码转String，注意调整自己的编码格式GBK、UTF-8
         channel.pipeline().addLast(new StringDecoder(Charset.forName("GBK")));
-        //编码器，发数据时起作用
+        // 解码转String，注意调整自己的编码格式GBK、UTF-8
         channel.pipeline().addLast(new StringEncoder(Charset.forName("GBK")));
-        //在管道中添加我们自己的接收数据实现方法
-        channel.pipeline().addLast(new MyServerHandler());
+        // 在管道中添加我们自己的接收数据实现方法
+        channel.pipeline().addLast(new MyClientHandler());
     }
 }
---
+++ src/main/java/org/zhiyu/coding/NettyClient.java
@@ -0,0 +1,31 @@
+package org.zhiyu.coding;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class NettyClient {
+    public static void main(String[] args) {
+        new NettyClient().connect("192.168.2.26", 7397);
+    }
+
+    private void connect(String inetHost, int inetPort) {
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(workerGroup);
+            b.channel(NioSocketChannel.class);
+            b.option(ChannelOption.AUTO_READ, true);
+            b.handler(new MyChannelInitializer());
+            ChannelFuture f = b.connect(inetHost, inetPort).sync();
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
